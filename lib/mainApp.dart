ffa914805f019a2baaba9f8772591960e287e324
erenkarakayaw <115037872+erenkarakayaw@users.noreply.github.com>
2023-09-22 13:57:33 +0300

    update
    yeni

---
+++ lib/mainApp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'mainScaffold.dart';
+
+class mainApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: mainScaffold(),
+    );
+  }
+
+
+}
---
+++ lib/mainScaffold.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class mainScaffold extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    ) ;
+  }
+
+}
